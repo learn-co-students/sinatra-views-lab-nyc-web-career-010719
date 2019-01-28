a630965657fb869bbc37fdd5d7a6da7f9fd88c37
mistermur <brian.j.murillo@gmail.com>
2019-01-28 13:16:39 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,24 @@
+# require 'DateTime'
+require 'Time'
 class App < Sinatra::Base
 
 	get '/' do
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		date = Time.new.to_date.strftime('%A, %B %d,  %Y')
+		erb :"/date", :locals => { :date => date }
+		
+		# erb :date
+	end
 
 end
